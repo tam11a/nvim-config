addd7116ac87d4b96ae34bd39afd3313e2a897c2
tam11a <ibrahimsadiktamim@gmail.com>
2025-05-17 14:29:53 +0000

    init app

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- local map = vim.keymap.set
+
+-- Move to window using the <ctrl> arrow keys
+-- map("n", "<C-Up>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+-- map("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+-- map("n", "<C-Left>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+-- map("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> hjkl keys
+-- map("n", "<C-h>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+-- map("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+-- map("n", "<C-k>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+-- map("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
---
+++ lua/plugins/gitui.lua
@@ -0,0 +1,31 @@
+return {
+  "mason-org/mason.nvim",
+  opts = { ensure_installed = { "gitui" } },
+  keys = {
+    {
+      "<leader>gG",
+      function()
+        Snacks.terminal({ "gitui" })
+      end,
+      desc = "GitUi (cwd)",
+    },
+    {
+      "<leader>gg",
+      function()
+        Snacks.terminal({ "gitui" }, { cwd = LazyVim.root.get() })
+      end,
+      desc = "GitUi (Root Dir)",
+    },
+  },
+  init = function()
+    -- delete lazygit keymap for file history
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimKeymaps",
+      once = true,
+      callback = function()
+        pcall(vim.keymap.del, "n", "<leader>gf")
+        pcall(vim.keymap.del, "n", "<leader>gl")
+      end,
+    })
+  end,
+}
