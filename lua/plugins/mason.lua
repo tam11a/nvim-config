2c9cbea77bc7ad3a9e3d5da0680420fb47132258
tam11a <ibrahimsadiktamim@gmail.com>
2025-05-18 14:02:16 +0600

    added: ts language configs

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,7 @@
+return {
+  "mason-org/mason.nvim",
+  opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    table.insert(opts.ensure_installed, "js-debug-adapter")
+  end,
+}
